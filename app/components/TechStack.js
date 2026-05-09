99a0d326a6f3e3703548de07fe669a30eac17144
David Alexander <davidalexander2411@gmail.com>
2026-05-09 19:56:29 +0700

    feat: updated TechStack

--- app/components/TechStack.js
+++ app/components/TechStack.js
@@ -1,10 +1,10 @@
 "use client";
-import { RiTailwindCssFill } from "react-icons/ri";
+
 import { FaReact } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
-import { RiSupabaseLine } from "react-icons/ri";
+
 import { FaGitAlt } from "react-icons/fa";
-import { SiLaravel, SiPostgresql, SiDocker, SiNodedotjs } from "react-icons/si";
+import { SiLaravel, SiPostgresql, SiDocker, SiNodedotjs, SiNestjs, SiMongodb } from "react-icons/si";
 import { useMouseMove } from '../hooks/useMouseMove';
 import { BiLogoTypescript } from "react-icons/bi";
 
@@ -13,7 +13,7 @@ function TechStackItem({ icon, name, iconScaleClass = "" }) {
   const { position, handleMouseMove, style } = useMouseMove();
 
   return (
-    <div 
+    <div
       className="text-3xl sm:text-4xl flex items-center gap-2 grays3bg py-1.5 sm:py-2 px-2.5 sm:px-3 lg:px-4 rounded-md relative overflow-hidden group"
       onMouseMove={handleMouseMove}
       style={style}
@@ -24,7 +24,7 @@ function TechStackItem({ icon, name, iconScaleClass = "" }) {
       <div className="text-xs sm:text-sm inter z-10 relative">
         {name}
       </div>
-      <div 
+      <div
         className="absolute inset-0 opacity-0 group-hover:opacity-50 transition-opacity duration-300 pointer-events-none"
         style={{
           background: `radial-gradient(circle at ${position.x}px ${position.y}px, rgba(255,255,255,0.3), transparent 90%)`
@@ -35,7 +35,7 @@ function TechStackItem({ icon, name, iconScaleClass = "" }) {
 }
 
 function TechStack() {
-  return(
+  return (
     <div className="pt-12 sm:pt-16">
       <h1 className="inter-extrabold text-3xl sm:text-[35px]">Tech Stack</h1>
       <h1 className="grays2 text-sm sm:text-base pb-3 sm:pb-4">Tools, resources and software I use.</h1>
@@ -45,15 +45,15 @@ function TechStack() {
             <TechStackItem icon={<BiLogoTypescript />} name="TypeScript" />
             <TechStackItem icon={<TbBrandNextjs />} name="Next.js" />
             <TechStackItem icon={<FaReact />} name="React" />
-            <TechStackItem icon={<SiLaravel />} name="Laravel"/>
-            <TechStackItem icon={<RiTailwindCssFill />} name="Tailwind" />
+            <TechStackItem icon={<SiLaravel />} name="Laravel" />
+            <TechStackItem icon={<SiNestjs />} name="Nest.js" iconScaleClass="text-[0.9em]" />
           </div>
           <div className="flex flex-wrap md:flex-nowrap md:justify-between gap-3 sm:gap-5">
             <TechStackItem icon={<SiNodedotjs />} name="Node.js" iconScaleClass="text-[0.93em]" />
-            <TechStackItem icon={<RiSupabaseLine />} name="Supabase" iconScaleClass="text-[0.93em]" />
+            <TechStackItem icon={<SiMongodb />} name="MongoDB" iconScaleClass="text-[0.93em]" />
             <TechStackItem icon={<FaGitAlt />} name="Git" iconScaleClass="text-[0.94em]" />
             <TechStackItem icon={<SiDocker />} name="Docker" iconScaleClass="text-[0.93em]" />
-            <TechStackItem icon={<SiPostgresql />} name="PostgreSQL" iconScaleClass="text-[0.8em]"/>
+            <TechStackItem icon={<SiPostgresql />} name="PostgreSQL" iconScaleClass="text-[0.8em]" />
           </div>
         </div>
       </div>
