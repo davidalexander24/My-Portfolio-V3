34e5623c7bbb246e22331e59806f4ffe72efd15f
David Alexander <davidalexander2411@gmail.com>
2026-06-10 23:27:59 +0700

    feat: correct GitHub link in Hero component for accuracy

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -46,7 +46,7 @@ export default function Hero() {
           </h1>
         </div>
         <div className="flex flex-row text-3xl sm:text-[40px] pt-2 sm:pt-3 gap-2 items-center">
-          <a className="hover:opacity-60 duration-300 ease-in-out" href="https://www.github.com/davidalexander2411"><IoLogoGithub /></a>
+          <a className="hover:opacity-60 duration-300 ease-in-out" href="https://www.github.com/davidalexander24"><IoLogoGithub /></a>
           <a className="text-[34px] sm:text-[44px] hover:opacity-60 duration-300 ease-in-out" href="https://www.linkedin.com/in/davidalexander24/"><AiOutlineLinkedin /></a>
           <a className="hover:opacity-60 duration-300 ease-in-out" href="https://www.instagram.com/david.alexander24"><FaInstagram /></a>
         </div>
